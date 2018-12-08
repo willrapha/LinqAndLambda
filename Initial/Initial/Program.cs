fd352e1165155f98213ca7b3048b326f6f612f51
unknown <willraphaspace@gmail.com>
2018-12-08 18:43:41 -0300

    first trainning linq

---
+++ Initial/Initial/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Initial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            // Where
+            foreach (int number in numbers.Where(n => n > 4))
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
