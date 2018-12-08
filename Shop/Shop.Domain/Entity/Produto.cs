529b72a7bc7377da820bcd8a1aa47ff4eccf12ba
unknown <willraphaspace@gmail.com>
2018-12-08 19:49:08 -0300

    complete foreach

---
+++ Shop/Shop.Domain/Entity/Produto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shop.Domain.Entity
+{
+    public class Produto
+    {
+        // IEnumerable nao tem o Add do List
+        private List<Produto> _produtos;
+
+        public Produto()
+        {
+            _produtos = new List<Produto>();
+        }
+
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public int Quantidade { get; set; }
+        public int Valor { get; set; }
+        public DateTime DataVencimento { get; set; }
+
+        public List<Produto> Listar()
+        {
+            _produtos.Add(new Produto { Id = Guid.NewGuid(), Nome = "Banana", Quantidade = 5, Valor = 19, DataVencimento = DateTime.Now.AddDays(5) });
+            _produtos.Add(new Produto { Id = Guid.NewGuid(), Nome = "Morango", Quantidade = 4, Valor = 12, DataVencimento = DateTime.Now.AddDays(5) });
+            _produtos.Add(new Produto { Id = Guid.NewGuid(), Nome = "Pera", Quantidade = 2, Valor = 15, DataVencimento = DateTime.Now.AddDays(5) });
+            _produtos.Add(new Produto { Id = Guid.NewGuid(), Nome = "Abacaxi", Quantidade = 1, Valor = 4, DataVencimento = DateTime.Now.AddDays(5) });
+            _produtos.Add(new Produto { Id = Guid.NewGuid(), Nome = "Melão", Quantidade = 35, Valor = 9, DataVencimento = DateTime.Now.AddDays(5) });
+
+            return _produtos;
+        }
+    }
+}
---
+++ Shop/Shop/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using Shop.Domain.Entity;
+
+namespace Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Where
+            var produtos = new Produto().Listar().Where(p => p.Valor > 4);
+            foreach (var item in produtos)
+            {
+                Console.WriteLine(item.Nome);
+            }
+
+            // ForEach
+            // Unica operação
+            new Produto().Listar().ForEach(p => Console.WriteLine(p.Nome));
+
+            // Para mais de uma operação colocar tudo entre chave
+            new Produto().Listar().ForEach(p =>
+                {
+                    Console.WriteLine(p.Nome);
+                    Console.WriteLine(p.Valor);
+                }
+            );
+
+            // o Where retorna um IEnumerable por isso utilizamos o .ToList() devido o ForEach aceitar apenas List
+            new Produto().Listar().Where(p => p.Valor > 4).ToList().ForEach(p =>
+                {
+                    Console.WriteLine(p.Nome);
+                    Console.WriteLine(p.Valor);
+                }
+            );
+
+            Console.ReadKey();
+        }
+    }
+}
